19d6e56a6d3ec13ee854619939a406fce2ffee3d
DejanS <dsimic94@gmail.com>
2018-04-22 23:28:59 +0200

    Task12

--- nba/app/Providers/AppServiceProvider.php
+++ nba/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+Use App\Team;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('partials.master', function($view)
+        {
+        $view->with('teams', Team::get());
+        });
     }
 
     /**
--- nba/resources/views/partials/master.blade.php
+++ nba/resources/views/partials/master.blade.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>@yield('title')</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/app.css">
   </head>
 
   <body>
@@ -20,6 +21,13 @@
           {{$message}}
         </div>
         @endif
+
+         <div class="sidenav">
+      @foreach($teams as $team)
+        <a href="{{ route('show-team', ['team' => $team->name]) }}">{{$team->name}}</a>
+      @endforeach
+    </div>
+    
     <div class="container">
     @yield('content')
     <nav class="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
